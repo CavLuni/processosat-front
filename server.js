b13330b2bf9e02f0d51bd0f052376a3bed537020
Denis Luniere <denis.luniere@gmail.com>
2021-06-21 22:34:09 -0300

    CORS

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 4200;
 
 app.use(express.static(__dirname + '/dist/angular-material-base'));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+  
+
 app.get('/*',(req,res) => {
     res.sendFile(__dirname + '/dist/angular-mateial-base/index.html')
 });
